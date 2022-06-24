7cf6db7d8bec6aaf402148424b5ec8d7022a2fe0
Alexsander <alexsandergutierrez@gmail.com>
2022-06-23 21:57:45 -0300

    first commit

---
+++ TESTE_CRIPTO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRIPTO_API;
+
+namespace TESTE_CRIPTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+            string input = Console.ReadLine();
+            string d = Cripto.Codificar(input);
+            Console.Write(d);
+
+            Console.ReadKey();
+        }
+    }
+}
